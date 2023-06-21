8b4cab71e2ab7dc8163d08bc537e31d381e7bfa3
John Pinto <59218902+pj8912@users.noreply.github.com>
2023-06-21 14:58:26 +0530

    Update User.py
    created User Model

--- src/Models/User.py
+++ src/Models/User.py
@@ -1,5 +1,19 @@
-
-
 class User:
-    pass
+    def __init__(self, cursor, cnx):
+        self.table = "users"
+        self.cursor = cursor
+        self.cnx = cnx
 
+    def create_user(self, flname,uname,pwd):
+        sql = f"INSERT INTO users(fullname, user_uname, user_pwd) VALUES(%s,%s,%s)"
+        self.cursor.execute(sql, (flname,uname,pwd))
+        self.cnx.commit()
+
+    def get_user_by_username(self,uname):
+        sql = f"SELECT * FROM {self.table} where user_uname=%s"
+        self.cursor.execute(sql,uname)
+        result = self.cursor.fetchone()
+        return result
+
+
+    
